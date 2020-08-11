7f560a0426adea001d191c8846e205c5436374c3
李政諺 <68540734+Ken970731@users.noreply.github.com>
2020-08-11 20:42:31 +0800

    Add files via upload

---
+++ hw4.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import random
+
+ans=random.randint(1,20)
+
+time=0 
+
+while time < 5:
+    guess=inpput('請輸入一個數字:')
+    guess=int(guess)
+    
+    
+    if guess>ans:
+    if time ==4:
+        print("你已")
+
+    
+    
---
+++ turtle.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 10 17:06:18 2020
+
+@author: ASUS-UX21
+"""
+
+import turtle
+import time
+
+
+eason=turtle.Turtle()
+
+eason.color('purple')
+eason.shape('turtle')
+
+time.sleep(1)
+eason.forward(100)
+time.sleep(1)
+eason.forward(100)
+time.sleep(1)
+eason.forward(100)
+time.sleep(1)
+eason.forward(100)
+              
